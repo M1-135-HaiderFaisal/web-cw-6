736e211aebd7032d88986962a428571a8b76cedf
M1-135-HaiderFaisal <76619433+M1-135-HaiderFaisal@users.noreply.github.com>
2021-01-05 13:34:04 +0300

    تم الحل

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,13 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+let name =prompt(`What your name?`);
+   
+
+console.log(name)
+function sayHello(){
+    alert(`Hello,${Name}`);
+}
+if(name){
+    sayHello()
+}else{
+    alert(`invalid input`)
+}
